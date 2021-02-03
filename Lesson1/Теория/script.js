b5949c34048930e601f3f4b1ad12aa5652bffce0
Alexandr Ovcharenko <alex980127@gmail.com>
2021-02-03 15:25:24 +0300

    Lesson1/Теория

---
+++ Lesson1/Теория/script.js
@@ -0,0 +1,138 @@
+'use strict';
+
+// //1 часть
+// var leftBorderWidth = 1;
+// {
+//     let second = 2;
+// }
+
+// const pi = 3.13;
+
+// //console.log(leftBorderWidth);
+// console.log(second);
+
+
+
+// //2 часть
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true; 
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+// console.log('string*9');
+
+// let something;
+// console.log(something);
+
+// let persone = {
+//     name      : "John",
+//     age       : 25,
+//     isMarried : false
+// };
+
+// // console.log(persone.name);
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','ornage.jpg','apple.bmp']
+
+// console.log(arr[2])
+
+
+
+// //3 часть
+// // 3 4 6
+// // 'string', 'string', 'string';
+// // true / false
+
+// // let a;
+// // console.log(a);
+
+// // const obj = {
+// //     name: 'Alex'
+// // }
+// //             0 1 2 3
+// // const arr = [1, 3, 5, 6];
+
+// // - to String
+
+// // 1)
+// console.log(typeof(String(4)));
+// // 2)
+// console.log(typeof("" + false));
+// console.log('https://vk.com/catalog/' + 5);
+
+// // - to Number
+
+// // 1)
+// console.log(typeof(Number('5')));
+// // 2)
+// console.log(typeof(5 + + '5'));
+// // 3) 
+// console.log(typeof(parseInt('15px', 10)));
+
+// let ans = +prompt("Hello?", '');
+
+// // 0, '', null, undefined, NaN
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log("Working...")
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log("Working...")
+// }
+
+// // 2)
+// console.log(typeof(Boolean('5')));
+// // 3)
+// console.log(typeof(!!('5')));
+
+
+// // 4 часть
+// // alert("Hello World!");
+
+// // let answer = confirm("Are you here?");
+
+// // console.log(answer)
+
+// // let answer = prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(null));
+
+
+// // 5 часть
+
+// // console.log(typeof(answer));
+
+// // console.log("arr" + " - object");
+// // console.log(4 + +" - object");
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// // console.log(++incr);
+// // console.log(--decr);
+
+// console.log(incr--);
+// console.log(decr++);
+
+// console.log(5%2);
+// console.log("2" === 2);
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
