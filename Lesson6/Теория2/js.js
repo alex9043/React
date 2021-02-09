0dfa30911bfa1508c55d58f5c37d2e3f87b3531e
Alexandr Ovcharenko <alex980127@gmail.com>
2021-02-09 14:11:15 +0300

    finished part 1 js

---
+++ Lesson6/Теория2/js.js
@@ -0,0 +1,65 @@
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', function(){
+    let box = document.querySelector('.box');
+    // box.addEventListener('touchstart', function(e){
+    //     e.preventDefault();
+    //     console.log("Red box: touchstart");
+    //     console.log(e.target);
+    //     console.log(e.touches[0].target);
+    //     console.log(e.changedTouches);
+    //     console.log(e.targetTouches);
+    // });
+
+    box.addEventListener('touchmove', function(e){
+        e.preventDefault();
+        console.log("Red box: " + e.touches[0].pageX);
+    });
+
+    // box.addEventListener('touchend', function(e){
+    //     e.preventDefault();
+    //     console.log("Red box: touchend");
+    // });
+
+
+    // new RegExp('pattern', 'flags');
+    // /pattern/
+
+    // let ans = prompt('Введите ваше имя');
+
+    // let reg = /n/gi;
+
+    // // // console.log(ans.search(reg));
+    // // console.log(ans.match(reg));
+    // console.log(reg.test(ans));
+
+    //i маленькая или большая буква
+    //g все вхождения
+    //m многострочность
+
+    // \d Цифры \D не число
+    // \w Буквы
+    // \s Пробелы
+
+    // let pass = prompt('Введите пароль');
+
+    // console.log(pass.replace(/./g, "*"));
+    // alert('12-34-56'.replace(/-/g, ':'));
+
+    // let ans = prompt('Введите ваше имя');
+
+    // let reg = /\d/g;
+
+    // console.log(ans.match(reg));
+
+    let str = 'My name is/ R2D2';
+
+    console.log(str.match(/\//i));
+
+
+});
